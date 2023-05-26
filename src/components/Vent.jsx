a0d5cc81228b336bee7f848b0a2cfb42398e83cc
m-shao <s201083816@ddsbstudent.ca>
2023-05-26 12:34:39 -0400

    added vent unscrew:

--- src/components/Vent.jsx
+++ src/components/Vent.jsx
@@ -1,60 +1,63 @@
-import React from 'react'
+import { useState } from 'react'
 
 function Vent() {
-  return (
-    <div className='w-[40vw]'>
-        <svg className='w-full' viewBox="0 0 1610 918" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g id="vent-page">
-                <g id="vent">
-                    <rect id="Rectangle 89" x="0.0874023" width="1609.83" height="918" fill="#454545" />
-                    <rect id="Rectangle 90" x="43.9922" y="120.977" width="1522.02" height="718.744" fill="#1F1F1F" />
-                    <rect id="Rectangle 91" x="73.2617" y="156.557" width="1463.48" height="654.698" fill="#454545" />
-                    <path id="Rectangle 92" d="M102.53 227.72V199.255L1507.47 199.255V227.72L102.53 227.72Z" fill="#1F1F1F" />
-                    <rect id="Rectangle 93" x="102.53" y="306" width="28.4651" height="1404.94"
-                        transform="rotate(-90 102.53 306)" fill="#1F1F1F" />
-                    <rect id="Rectangle 94" x="102.53" y="384.28" width="28.4651" height="1404.94"
-                        transform="rotate(-90 102.53 384.28)" fill="#1F1F1F" />
-                    <rect id="Rectangle 95" x="102.53" y="462.557" width="28.4651" height="1404.94"
-                        transform="rotate(-90 102.53 462.557)" fill="#1F1F1F" />
-                    <rect id="Rectangle 96" x="102.53" y="540.837" width="28.4651" height="1404.94"
-                        transform="rotate(-90 102.53 540.837)" fill="#1F1F1F" />
-                    <rect id="Rectangle 97" x="102.53" y="619.117" width="28.4651" height="1404.94"
-                        transform="rotate(-90 102.53 619.117)" fill="#1F1F1F" />
-                    <rect id="Rectangle 98" x="102.53" y="697.397" width="28.4651" height="1404.94"
-                        transform="rotate(-90 102.53 697.397)" fill="#1F1F1F" />
-                    <rect id="Rectangle 99" x="102.53" y="775.674" width="28.4651" height="1404.94"
-                        transform="rotate(-90 102.53 775.674)" fill="#1F1F1F" />
+
+    const [screwsOpen, setScrewsOpen] = useState([])
+
+    return (
+        <div className='w-[40vw]'>
+            <svg className='w-full' viewBox="0 0 1610 918" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g id="vent-page">
+                    <g id="vent">
+                        <rect id="Rectangle 89" x="0.0874023" width="1609.83" height="918" fill="#454545" />
+                        <rect id="Rectangle 90" x="43.9922" y="120.977" width="1522.02" height="718.744" fill="#1F1F1F" />
+                        <rect id="Rectangle 91" x="73.2617" y="156.557" width="1463.48" height="654.698" fill="#454545" />
+                        <path id="Rectangle 92" d="M102.53 227.72V199.255L1507.47 199.255V227.72L102.53 227.72Z" fill="#1F1F1F" />
+                        <rect id="Rectangle 93" x="102.53" y="306" width="28.4651" height="1404.94"
+                            transform="rotate(-90 102.53 306)" fill="#1F1F1F" />
+                        <rect id="Rectangle 94" x="102.53" y="384.28" width="28.4651" height="1404.94"
+                            transform="rotate(-90 102.53 384.28)" fill="#1F1F1F" />
+                        <rect id="Rectangle 95" x="102.53" y="462.557" width="28.4651" height="1404.94"
+                            transform="rotate(-90 102.53 462.557)" fill="#1F1F1F" />
+                        <rect id="Rectangle 96" x="102.53" y="540.837" width="28.4651" height="1404.94"
+                            transform="rotate(-90 102.53 540.837)" fill="#1F1F1F" />
+                        <rect id="Rectangle 97" x="102.53" y="619.117" width="28.4651" height="1404.94"
+                            transform="rotate(-90 102.53 619.117)" fill="#1F1F1F" />
+                        <rect id="Rectangle 98" x="102.53" y="697.397" width="28.4651" height="1404.94"
+                            transform="rotate(-90 102.53 697.397)" fill="#1F1F1F" />
+                        <rect id="Rectangle 99" x="102.53" y="775.674" width="28.4651" height="1404.94"
+                            transform="rotate(-90 102.53 775.674)" fill="#1F1F1F" />
+                    </g>
+                    <g id="screws">
+                        <g id="screw-1" onClick={() => {setScrewsOpen([...screwsOpen, 1])}} className={(screwsOpen.includes(1) && "transition-all duration-1000 opacity-0")}>
+                            <circle id="Ellipse 26" cx="52.929" cy="45.873" r="26.9841" fill="#353535" />
+                            <rect id="Rectangle 112" x="51.3101" y="30.2222" width="3.2381" height="31.3016" fill="#1F1F1F" />
+                            <rect id="Rectangle 113" x="37.2783" y="47.4919" width="3.2381" height="31.3016"
+                                transform="rotate(-90 37.2783 47.4919)" fill="#1F1F1F" />
+                        </g>
+                        <g id="screw-2" onClick={() => {setScrewsOpen([...screwsOpen, 2])}} className={(screwsOpen.includes(2) && "transition-all duration-1000 opacity-0")}>
+                            <circle id="Ellipse 26_2" cx="1563.5" cy="45.873" r="26.9841" fill="#353535" />
+                            <rect id="Rectangle 112_2" x="1561.88" y="30.2222" width="3.2381" height="31.3016" fill="#1F1F1F" />
+                            <rect id="Rectangle 113_2" x="1547.85" y="47.4919" width="3.2381" height="31.3016"
+                                transform="rotate(-90 1547.85 47.4919)" fill="#1F1F1F" />
+                        </g>
+                        <g id="screw-3" onClick={() => {setScrewsOpen([...screwsOpen, 3])}} className={(screwsOpen.includes(3) && "transition-all duration-1000 opacity-0")}>
+                            <circle id="Ellipse 26_3" cx="1565.12" cy="877.524" r="26.9841" fill="#353535" />
+                            <rect id="Rectangle 112_3" x="1563.5" y="861.873" width="3.2381" height="31.3016" fill="#1F1F1F" />
+                            <rect id="Rectangle 113_3" x="1549.47" y="879.143" width="3.2381" height="31.3016"
+                                transform="rotate(-90 1549.47 879.143)" fill="#1F1F1F" />
+                        </g>
+                        <g id="screw-4" onClick={() => {setScrewsOpen([...screwsOpen, 4])}} className={(screwsOpen.includes(4) && "transition-all duration-1000 opacity-0")}>
+                            <circle id="Ellipse 26_4" cx="51.3103" cy="878.063" r="26.9841" fill="#353535" />
+                            <rect id="Rectangle 112_4" x="49.6909" y="862.413" width="3.2381" height="31.3016" fill="#1F1F1F" />
+                            <rect id="Rectangle 113_4" x="35.6592" y="879.683" width="3.2381" height="31.3016"
+                                transform="rotate(-90 35.6592 879.683)" fill="#1F1F1F" />
+                        </g>
+                    </g>
                 </g>
-                <g id="screws">
-                    <g id="screw-1">
-                        <circle id="Ellipse 26" cx="52.929" cy="45.873" r="26.9841" fill="#353535" />
-                        <rect id="Rectangle 112" x="51.3101" y="30.2222" width="3.2381" height="31.3016" fill="#1F1F1F" />
-                        <rect id="Rectangle 113" x="37.2783" y="47.4919" width="3.2381" height="31.3016"
-                            transform="rotate(-90 37.2783 47.4919)" fill="#1F1F1F" />
-                    </g>
-                    <g id="screw-2">
-                        <circle id="Ellipse 26_2" cx="1563.5" cy="45.873" r="26.9841" fill="#353535" />
-                        <rect id="Rectangle 112_2" x="1561.88" y="30.2222" width="3.2381" height="31.3016" fill="#1F1F1F" />
-                        <rect id="Rectangle 113_2" x="1547.85" y="47.4919" width="3.2381" height="31.3016"
-                            transform="rotate(-90 1547.85 47.4919)" fill="#1F1F1F" />
-                    </g>
-                    <g id="screw-3">
-                        <circle id="Ellipse 26_3" cx="1565.12" cy="877.524" r="26.9841" fill="#353535" />
-                        <rect id="Rectangle 112_3" x="1563.5" y="861.873" width="3.2381" height="31.3016" fill="#1F1F1F" />
-                        <rect id="Rectangle 113_3" x="1549.47" y="879.143" width="3.2381" height="31.3016"
-                            transform="rotate(-90 1549.47 879.143)" fill="#1F1F1F" />
-                    </g>
-                    <g id="screw-4">
-                        <circle id="Ellipse 26_4" cx="51.3103" cy="878.063" r="26.9841" fill="#353535" />
-                        <rect id="Rectangle 112_4" x="49.6909" y="862.413" width="3.2381" height="31.3016" fill="#1F1F1F" />
-                        <rect id="Rectangle 113_4" x="35.6592" y="879.683" width="3.2381" height="31.3016"
-                            transform="rotate(-90 35.6592 879.683)" fill="#1F1F1F" />
-                    </g>
-                </g>
-            </g>
-        </svg>
-    </div>
-  )
+            </svg>
+        </div>
+    )
 }
 
 export default Vent
